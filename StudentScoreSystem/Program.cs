81fc7231fcb5ba9eaf7e4b174b357b20b7c71d69
L3on4rd0wen <l3on4rd30@gmail.com>
2026-01-08 14:20:11 +0700

    added if-else branch

--- StudentScoreSystem/Program.cs
+++ StudentScoreSystem/Program.cs
@@ -3,8 +3,21 @@ int score;
 string name;
 
 Console.WriteLine("Student Name:");
-
 name = Console.ReadLine();
+
+Console.WriteLine("Student Score:");
+score = int.Parse(Console.ReadLine());
+
+if (score >= 75)
+{
+    Console.WriteLine($"You Passed");
+}
+else
+{
+    Console.WriteLine($"You Failed");
+}
+
+
 
 /* 
  i  | i i
